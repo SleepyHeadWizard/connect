adb893db498683c90110e3e77f807ea56f613717
SleepyHeadWizard <117934973+SleepyHeadWizard@users.noreply.github.com>
2026-03-30 14:16:48 +0530

    Update Vite config base path
    Set base path for Vite configuration to '/connect/'.

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ import react from '@vitejs/plugin-react'
 //     open: true
 //   }
 // })
+export default defineConfig({
+  plugins: [react()],
+  base: '/connect/', // Replace with your actual repository name
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
